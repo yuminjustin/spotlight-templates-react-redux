136aea509933807adcc2b2f34f319ffeff4da25b
justin lee <yuminjustin@163.com>
2018-09-07 15:03:33 +0800

    MiniCssExtractPlugin

--- template/build/common/utils.js
+++ template/build/common/utils.js
@@ -1,6 +1,7 @@
 /* 公共方法 */
+var path = require('path')
 var origin_config = require("../config")
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var MiniCssExtractPlugin = require("mini-css-extract-plugin");
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 
@@ -56,63 +57,21 @@ exports.HtmlWPMaker = function (config) {
     return arr;
 }
 
-/* webpack styles loader maker */
-var cssLoaders = function (options) {
-    options = options || {}
-
-    var cssLoader = {
-            loader: 'css-loader',
-            options: {
-                minimize: process.env.NODE_ENV === 'production',
-                sourceMap: options.sourceMap
-            }
-        },
-        generateLoaders = function (loader, loaderOptions) {
-            var loaders = [cssLoader]
-            if (loader) {
-                loaders.push({
-                    loader: loader + '-loader',
-                    options: Object.assign({}, loaderOptions, {
-                        sourceMap: options.sourceMap
-                    })
-                })
-            }
-
-            if (options.extract) {
-                return ExtractTextPlugin.extract({
-                    use: loaders,
-                    publicPath: '../../', // 解决二级目录问题
-                    fallback: 'style-loader'
-                })
-            } else {
-                return ['style-loader'].concat(loaders)
-            }
-
-        }
-
-    return {
-        css: generateLoaders(),
-        less: generateLoaders('less'),
-        sass: generateLoaders('sass', {
-            indentedSyntax: true
-        }),
-        scss: generateLoaders('sass'),
-        stylus: generateLoaders('stylus'),
-        styl: generateLoaders('stylus')
-    }
+exports.styleLoaders = function (isdev) {
+    isdev = isdev || !1;
+    return [{
+        test: /\.css$/,
+        use: [
+            isdev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader'
+        ]
+    }]
 }
 
-
-// less sass css ...  loders
-exports.styleLoaders = function (options) {
-    var output = [],
-        loaders = cssLoaders(options)
-    for (var extension in loaders) {
-        var loader = loaders[extension]
-        output.push({
-            test: new RegExp('\\.' + extension + '$'),
-            use: loader
-        })
+exports.filenames = function (type) {
+    var _dir = './static';
+    return {
+        filename: path.posix.join(_dir, type + '/[name].[chunkhash].' + type),
+        chunkFilename: path.posix.join(_dir, type + '/[name].[chunkhash].' + type)
     }
-    return output
 }
